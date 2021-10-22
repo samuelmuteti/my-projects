644edc936c8b5da3069f267182fb29cfa706ef27
samuelmuteti <91966864+samuelmuteti@users.noreply.github.com>
2021-10-22 12:00:08 +0300

    Add files via upload

---
+++ increment and decrement operators.c
@@ -0,0 +1,14 @@
+// Working of increment and decrement operators
+#include <stdio.h>
+int main()
+{
+    int x = 30, y = 300;
+    float z = 30.5, d = 300.5;
+
+    printf("++x = %d \n", ++x);
+    printf("--y = %d \n", --y);
+    printf("++z = %f \n", ++z);
+    printf("--d = %f \n", --d);
+
+    return 0;
+}
