dd36ac3713fc901184d40713e894115f1a745efd
samuelmuteti <91966864+samuelmuteti@users.noreply.github.com>
2021-10-29 12:41:32 +0300

    Add files via upload

---
+++ Local variables.c
@@ -0,0 +1,12 @@
+/*local variables*/
+#include <stdio.h>
+int main()
+{
+    int a,b,c; //local variables
+    a=15;
+    b=10;
+    c=a*b;
+    printf("value of a = %d, b = %d and c = %d\n", a, b, c);
+    return 0;
+
+}
